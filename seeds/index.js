c849967528a741b137bfadf0a525c23e163ab380
bvlcsae <ayangelv@163.com>
2023-03-16 15:26:05 +0800

    Styling Campgrounds Index

--- seeds/index.js
+++ seeds/index.js
@@ -29,6 +29,7 @@ const seedDB = async () => {
             author: '5f5c330c2cd79d538f2c66d9',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
             price,
             geometry: {
