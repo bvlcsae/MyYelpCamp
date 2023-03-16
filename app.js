9572830fc576c042ef660267ea38c101ced21aaa
bvlcsae <ayangelv@163.com>
2023-03-16 14:35:29 +0800

    update app

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-const morgan = require("morgan");
-const ejsMate = require('ejs-mate')
+const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -19,21 +18,29 @@ db.once("open", () => {
 });
 
 const app = express();
-app.engine('ejs', ejsMate)
+
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(morgan("common"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
@@ -46,52 +53,20 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
   res.render("campgrounds/edit", { campground });
 });
 
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.delete("/campgrounds/:id", async (req, res) => {
-  await Campground.findByIdAndDelete(req.params.id);
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
   res.redirect("/campgrounds");
 });
 
-app.get("/new", async (req, res) => {
-  res.render("campgrounds/new");
-});
-
-app.put("/campgrounds/:id", async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(
-    id,
-    req.body.campground
-  );
-  res.redirect(`/campgrounds/${campground._id}`);
-});
-
-app.post("/campgrounds", async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
-});
-
-const verifyPassword = (req, res, next) => {
-  const { password } = req.query;
-  if (password === "chickennugget") {
-    next();
-    return;
-  }
-  res.send("SORRY YOU NEED A PASSWORD!!!");
-};
-
-app.get("/dogs", (req, res) => {
-  console.log(`REQUEST TIME: ${req.requestTime}`);
-  res.send("WOOF WOOF!");
-});
-
-app.get("/secret", verifyPassword, (req, res) => {
-  res.status(200).send("MY SECRET IS: Some xxxxxx");
-});
-
-app.use((req, res) => {
-  res.status(404).send("NOT FOUND!");
-});
-
 app.listen(3000, () => {
-  console.log("Server on port 3000");
+  console.log("Serving on port 3000");
 });
